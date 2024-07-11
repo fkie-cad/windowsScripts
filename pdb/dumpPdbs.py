284d4511a2d39299e2cf680c2b2d42e4b2c20d0d
Henning Braun <henning.braun@fkie.fraunhofer.de>
2024-07-11 13:07:19 +0200

    updates, fixes, dumpPdbs

--- pdb/dumpPdbs.py
+++ pdb/dumpPdbs.py
@@ -7,25 +7,28 @@ import requests
 import sys
 
 SYMBOLS_SERVER = 'https://msdl.microsoft.com/download/symbols'
+SYSTEM32 = 'C:\\Windows\\System32'
+DRIVERS = 'C:\\Windows\\System32\\drivers'
 
 PROGRAM_DESCRIPTION = 'A PDB file download tool.'
 PROGRAM_NAME = 'loadPdb'
-PROGRAM_VERSION = '1.0.1'
+PROGRAM_VERSION = '1.0.0'
 
 
 def loadPdb(file_path, target_dir):
-        
+
+    file_path = Path(file_path).expanduser()
+    print("path: %s" % file_path);
+    print("target_dir: %s" % file_path);
+    
+    if not file_path.is_file():
+        return -1
+    
     file_stem = Path(file_path).stem
     
-    print("path: %s" % file_path);
-    print("  stem: %s" % file_stem);
+    print("stem: %s" % file_stem);
 
-    try:
-        pe = pefile.PE(file_path, fast_load=True)
-    except pefile.PEFormatError:
-        print("  [e] pe parsing error")
-        return -2
-        
+    pe = pefile.PE(file_path, fast_load=True)
     pe.parse_data_directories()
     for directory in pe.DIRECTORY_ENTRY_DEBUG:
         debug_entry = directory.entry
@@ -34,28 +37,53 @@ def loadPdb(file_path, target_dir):
             guid = f'{debug_entry.Signature_Data1:08x}'
             guid += f'{debug_entry.Signature_Data2:04x}'
             guid += f'{debug_entry.Signature_Data3:04x}'
-            guid += f'{debug_entry.Signature_Data4:02x}'
-            guid += f'{debug_entry.Signature_Data5:02x}'
-            guid += f'{int.from_bytes(debug_entry.Signature_Data6, byteorder="big"):08x}'
+            guid += f'{int.from_bytes(debug_entry.Signature_Data4, byteorder="big"):016x}'
             guid = guid.upper()
             url = f'{SYMBOLS_SERVER}/{pdb_file}/{guid}{debug_entry.Age:x}/{pdb_file}'
-            print("  url: %s" % url);
+            print("url: %s" % url);
             
             target_path = f'{target_dir}/{guid}-{debug_entry.Age:x}-{file_stem}.pdb'
             print("  target_path: %s" % target_path);
             r = requests.get(url)
             if r.status_code != 200:
-                print("  [e] Url not found! (%u)" % r.status_code);
+                print("  [e] url not found! (%u)" % r.status_code);
                 break
                 
             f = open(target_path, 'wb')
-            f.write(r.content)
+            f.write(pdb_file.content)
             f.close()
-            print("  [x]  done")
             
             break
             
     return 0
+
+
+def dump(lm_path, target_dir):
+    lm_path = Path(lm_path).expanduser()
+    print("lm_path: %s" % lm_path);
+    print("target_dir: %s" % lm_path);
+    
+    # Using readlines()
+    sm_file = open(lm_path, 'r')
+    sm_lines = sm_file.readlines()
+      
+    count = 0
+    # Strips the newline character
+    for line in sm_lines:
+        count += 1
+        # ~ print("Line{}: {}".format(count, line.strip()))
+        
+        x = line.split()
+        # ~ print("%s" % str(x))
+        if len(x) == 4:
+            print("path: %s" % x[3])
+            file_path = f'{DRIVERS}\\{x[3]}'
+            s = loadPdb(file_path, target_dir)
+            if s != 0:
+                file_path = f'{SYSTEM32}\\{x[3]}'
+                s = loadPdb(file_path, target_dir)
+            if s != 0:
+                print("[e] Did not pdb for %s" % file_path)
 
 
 def check_arguments():
@@ -64,10 +92,7 @@ def check_arguments():
     parser.usage = 'python %(prog)s [options] file1 [file2 ...]'
     parser.add_argument('-v', '--version', action='version', version='{} {}'.format(PROGRAM_NAME, PROGRAM_VERSION))
     parser.add_argument('-t', '--target', help='Target dir to store the downloaded files.', default="~/Downloads", type=str, required=False)
-    parser.add_argument('-f', '--filetype', help='Filetype filter for a directory.', default="*", type=str, required=False)
-    parser.add_argument('-r', '--recursive', help='Iterate the files of a directory recursively.', action='store_true', required=False)
-    # parser.add_argument('-d', '--debug-print', help='Show debug prints.', action='store_true', required=False)
-    parser.add_argument('files', nargs='+', help='A list of files or direcotry pathes.', default=None, type=str)
+    parser.add_argument('-i', '--input', help='Input file with modules from a "windbg> lm sm f" command.', default="", type=str, required=True)
 
     return parser.parse_args()
 
@@ -77,35 +102,16 @@ if __name__ == '__main__':
     if args is None:
         sys.exit()
 
-    files = args.files
-    number_of_files = len(files)
-    recursive = args.recursive
-    print("recursive: %s" % str(recursive));
-    file_type = args.filetype
-    print("file_type: %s" % file_type);
+    lm_file = args.input
     
-    target_dir = Path(args.target).expanduser()
-    print("target_dir: %s" % target_dir);
+    target_dir = args.target
+    target_dir = Path(target_dir).expanduser()
     
     if not target_dir.is_dir():
         raise IOError('IOError: Target dir does not exist: %s' % target_dir)
+        
+    dump(lm_file, target_dir)
     
-    for f in files:
-        file_path = Path(f).expanduser()
-        
-        if file_path.is_file():
-            s = loadPdb(file_path, target_dir)
-        elif file_path.is_dir():
-            if recursive:
-                gfilter = '**/*.%s' % file_type
-            else:
-                gfilter = './*.%s' % file_type
-            
-            for path in file_path.glob(gfilter):
-                s = loadPdb(path, target_dir)
-        else:
-            print("[e] Did not find %s" % f)
-        
 
     sys.exit()
 
